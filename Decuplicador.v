905954fd09b105b430d5a5f32e3e0a6ea26fd93b
PabloArias29 <92907590+PabloArias29@users.noreply.github.com>
2022-04-05 01:07:40 -0600

    Update and rename Archivos/Multiplicador to Decuplicador.v

--- Decuplicador.v
+++ Decuplicador.v
@@ -1,13 +1,10 @@
 module Mulplicador (A, G);
 
   input [3:0] A;
-  output [6:0] G;
-  reg [3:0] ten;
+  output [7:0] G;
   reg signed [6:0] mul;
-  always @*
-    begin
-      ten = 1010;
-      mul <= A*ten;
+  always @(A)*
+      mul = A * 4'b1010;
   assign G = mul;
 
 endmodule
